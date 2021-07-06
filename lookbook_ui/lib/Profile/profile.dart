a7ff520ca4a4bb4f4c4be2c9ef8456d86849200c
Darshan Hande <darshanhande11@gmail.com>
2021-07-06 16:27:01 +0530

    added profile options and a button to profile page

--- lookbook_ui/lib/Profile/profile.dart
+++ lookbook_ui/lib/Profile/profile.dart
@@ -1,3 +1,5 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
@@ -8,8 +10,8 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-
-  _buildPreferencesCard(String locationOfImage, String header, String desc, Color? backgroundColor) {
+  _buildPreferencesCard(String locationOfImage, String header, String desc,
+      Color? backgroundColor) {
     return Padding(
       padding: EdgeInsets.only(left: 15),
       child: InkWell(
@@ -21,7 +23,7 @@ class _ProfileState extends State<Profile> {
             color: backgroundColor,
           ),
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(12,12,0,0),
+            padding: const EdgeInsets.fromLTRB(12, 12, 0, 0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -43,23 +45,40 @@ class _ProfileState extends State<Profile> {
                   ),
                 ),
                 SizedBox(height: 20),
-                Text(
-                  header,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500
-                    )),
+                Text(header,
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
                 SizedBox(height: 8),
-                Text(
-                  desc,
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey[700]
-                    )),
+                Text(desc,
+                    style: TextStyle(fontSize: 14, color: Colors.grey[700])),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  _buildProfileOptions(IconData? icon, String name) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(25, 25, 12, 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Icon(
+            icon,
+            size: 25,
+            color: Colors.grey[700],
+          ),
+          SizedBox(
+            width: 30,
+          ),
+          Text(
+            name,
+            style: TextStyle(
+                fontWeight: FontWeight.w500, fontSize: 20, letterSpacing: 1.2),
+          )
+        ],
       ),
     );
   }
@@ -69,41 +88,39 @@ class _ProfileState extends State<Profile> {
     return ListView(
       children: [
         Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[200]
-          ),
+          decoration: BoxDecoration(color: Colors.grey[200]),
           child: Padding(
-          padding: EdgeInsets.fromLTRB(20, 30, 12, 30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Profile",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.grey[300],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(
-                    Icons.notifications_none_outlined,
-                    size: 32.0,
+            padding: EdgeInsets.fromLTRB(20, 30, 12, 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Profile",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
-              )
-            ],
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.grey[300],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.notifications_none_outlined,
+                      size: 32.0,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
         ),
         Container(
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(20,30,20,30),
+            padding: const EdgeInsets.fromLTRB(20, 30, 20, 30),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -117,23 +134,23 @@ class _ProfileState extends State<Profile> {
                     children: [
                       Text(
                         "Darshan Hande",
-                         style: TextStyle(
-                           fontSize: 23,
-                           fontWeight: FontWeight.w600,
-                           letterSpacing: 2,
-                         ),
+                        style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 2,
+                        ),
                       ),
                       SizedBox(
                         height: 10,
                       ),
                       Text(
                         "Edit Profile",
-                         style: TextStyle(
-                           decoration: TextDecoration.underline,
-                           fontSize: 15,
-                           fontWeight: FontWeight.w300,
-                           color: Colors.grey[800],
-                         ),
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.grey[800],
+                        ),
                       ),
                     ],
                   ),
@@ -143,17 +160,69 @@ class _ProfileState extends State<Profile> {
           ),
         ),
         SizedBox(
-          height:  15,
+          height: 15,
         ),
         Container(
           height: 150,
           child: ListView(
             scrollDirection: Axis.horizontal,
             children: [
-              _buildPreferencesCard('assets/Size.png','Size & Fit','Your Size & Fit Preferences',Colors.blue[100]),
-              _buildPreferencesCard('assets/mustache.png','Style','Your Style Preferences',Colors.pink[100]),
-              _buildPreferencesCard('assets/clothes-hanger.png','Closet','Your Closet',Colors.brown[100]),
+              _buildPreferencesCard('assets/Size.png', 'Size & Fit',
+                  'Your Size & Fit Preferences', Colors.blue[100]),
+              _buildPreferencesCard('assets/mustache.png', 'Style',
+                  'Your Style Preferences', Colors.pink[100]),
+              _buildPreferencesCard('assets/clothes-hanger.png', 'Closet',
+                  'Your Closet', Colors.brown[100]),
             ],
+          ),
+        ),
+        SizedBox(height: 30),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildProfileOptions(CupertinoIcons.cube_box, 'All Purchases'),
+            _buildProfileOptions(CupertinoIcons.info, 'FAQs'),
+            _buildProfileOptions(
+                CupertinoIcons.arrow_2_circlepath, 'Exchanges & Return Policy'),
+            _buildProfileOptions(CupertinoIcons.lock, 'Privacy Policy'),
+            _buildProfileOptions(CupertinoIcons.doc_text, 'About Us'),
+          ],
+        ),
+        SizedBox(height: 45),
+        Container(
+          margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: DottedBorder(
+            color: Colors.black,
+            strokeWidth: 1,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                elevation: 0,
+              ),
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      CupertinoIcons.headphones,
+                      size: 25,
+                      color: Colors.black,
+                    ),
+                    SizedBox(width: 20),
+                    Text(
+                      'Need Assistance ?',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ],
